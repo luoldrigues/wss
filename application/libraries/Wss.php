26036ddc7a9f9c6ec477401c851d8618d46b1be5
Luan Rodrigues <lrodrigues@gmail.com>
2016-03-31 23:39:30 -0300

    WSS Generate Token

--- application/libraries/Wss.php
+++ application/libraries/Wss.php
@@ -51,6 +51,26 @@ class Wss
 
     /**
      * PRIVATE METHOD
+     * Algorithm to generate a Token. It return a base64 encoded string.
+     *
+     * @param  [mixed]     $data       (required)      Token data to be encrypted.
+     * @return [string]
+     */
+    private function genToken($data)
+    {
+        $data = json_encode($data, true);
+
+        $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),MCRYPT_DEV_URANDOM);
+
+        $token = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, hash('sha256', $this->encryption_key, true), $data, MCRYPT_MODE_CBC, $iv);
+        $token .= $iv;
+
+        return urlencode(base64_encode($token));
+    }
+
+
+    /**
+     * PRIVATE METHOD
      * Prevents dependencies errors.
      *
      * @param  [array]      $params     (required)      Config file settings
