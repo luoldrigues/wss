0133a40f44f109fbef8a134aec109cc8dc74bdb3
Luan Rodrigues <lrodrigues@gmail.com>
2016-03-31 23:28:42 -0300

    WSS Library Init

---
+++ application/libraries/Wss.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Web Service Security (WSS)
+ *
+ * An open source library to help you protect your web service using token validation. Developed by Luan Rodrigues for CodeIgniter Framework 3.x.
+ *
+ * @author      Luan Rodrigues (luoldrigues@gmail.com)
+ * @version     Version 1.0
+ * @link        https://github.com/luoldrigues/wss
+ */
+
+class Wss
+{
+
+}
