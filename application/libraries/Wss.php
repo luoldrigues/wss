31d8360914dc8281660de867ab69e24115f6df62
Luan Rodrigues <lrodrigues@gmail.com>
2016-03-31 23:32:23 -0300

    WSS Requirements Validation

--- application/libraries/Wss.php
+++ application/libraries/Wss.php
@@ -13,5 +13,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Wss
 {
+    /**
+     * Define initial settings
+     */
+    public function __construct($params = array())
+    {
+        // Requirements validation
+        $this->requirements($params);
+    }
 
+
+    /**
+     * PRIVATE METHOD
+     * Prevents dependencies errors.
+     *
+     * @param  [array]      $params     (required)      Config file settings
+     * @return [none]
+     */
+    private function requirements($params)
+    {
+        // Mcrypt module dependence validation
+        if(!extension_loaded('mcrypt'))
+        {
+            throw new Exception('PHP mcrypt module is required');
+        }
+
+        // Encryption Key validation
+        if(!array_key_exists('encryption_key', $params))
+        {
+            throw new Exception('You must set your "encryption_key" in your library config file.');
+        }
+    }
 }
